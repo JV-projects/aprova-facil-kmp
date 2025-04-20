563026d5c20d52249d2b72a3ba72d91c231fbe02
carjooj <victor@4info.com.br>
2025-04-19 21:18:08 -0300

    feat: Inicalizando o projeto

---
+++ aprovafacil/composeApp/src/desktopMain/kotlin/edu/fatec/jvprojects/main.kt
@@ -0,0 +1,13 @@
+package edu.fatec.jvprojects
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "aprovafacil",
+    ) {
+        App()
+    }
+}
