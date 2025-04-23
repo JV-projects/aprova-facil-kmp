92a938195bc02156aa80e747ef15f052fc737cf6
carjooj <victor@4info.com.br>
2025-04-22 23:17:19 -0300

    feat: Adicionando as telas no NavHost

--- aprovafacil/composeApp/src/commonMain/kotlin/edu/fatec/jvprojects/App.kt
+++ aprovafacil/composeApp/src/commonMain/kotlin/edu/fatec/jvprojects/App.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import edu.fatec.jvprojects.composables.Formulario
 import edu.fatec.jvprojects.screens.ConsultaScreen
 import edu.fatec.jvprojects.screens.DetalheScreen
+import edu.fatec.jvprojects.screens.EditarScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -16,9 +18,11 @@ fun App() {
     val navController = rememberNavController()
 
     MaterialTheme {
-        NavHost(navController = navController, startDestination = "consulta"){
+        NavHost(navController = navController, startDestination = "formulario") {
+            composable("formulario") { Formulario(navController) }
             composable("consulta") { ConsultaScreen(navController) }
             composable("detalhes") { DetalheScreen(navController) }
+            composable("editar") { EditarScreen(navController) }
         }
     }
 }
