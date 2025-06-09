fb608fdc667362c0fee0e3484f765b5c232db9c3
carjooj <victor@4info.com.br>
2025-06-08 21:29:41 -0300

    feat: Criando e implementando conversão de base64 para Bitmap em todas as plataformas

---
+++ aprovafacil/composeApp/src/desktopMain/kotlin/edu/fatec/jvprojects/utils/extensions/Base64ToImageBitmap.desktop.kt
@@ -0,0 +1,7 @@
+package edu.fatec.jvprojects.utils.extensions
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import org.jetbrains.skia.Image
+
+actual fun ByteArray.toImageBitmap(): ImageBitmap? = Image.makeFromEncoded(this).toComposeImageBitmap()
